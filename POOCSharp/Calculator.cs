091e81f51ff6412f03ee29cb8ea3bdc06fbef701
Fernando Carrillo Lopez <kleyfat12@gmail.com>
2022-06-01 18:34:26 +0200

    Creating classes.

---
+++ POOCSharp/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOCSharp
+{
+    class Calculator
+    {
+        //Atributtes
+        public double a;
+        public double b;
+        public double r;
+
+
+        //Methods
+        public void Suma()
+        {
+            r = a + b;
+        }
+
+        public void Muestra()
+        {
+            Console.WriteLine("a={0}, b={1}, r={2}", a, b, r);
+        }
+    }
+}
--- POOCSharp/Program.cs
+++ POOCSharp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using POOCSharp;
+
+//Create an instance of calculator
+Calculator calculator = new Calculator();
+
+//We assign values
+calculator.a = 2;
+calculator.b = 3;
+
+//We operate and show the result
+calculator.Muestra();
+calculator.Suma();
+calculator.Muestra();
+
