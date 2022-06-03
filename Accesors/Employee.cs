102f282a580ba27aa1aab46151e77810368a7dcf
Fernando Carrillo Lopez <kleyfat12@gmail.com>
2022-06-03 07:54:20 +0200

    Accessors l

---
+++ Accesors/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accessor
+{
+    class Employee
+    {
+        private double salary;
+        private double tax;
+        private string name;
+
+        //Accessors (Getters and Setters)
+        public double GetSalary()
+        {
+            return salary;
+        }
+
+        public void SetSalary(double salary)
+        {
+            this.salary = salary;
+        }
+
+        public double GetTax()
+        {
+            return tax;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Salary = {0}, Tax = {1}, Name = {2}", salary, tax, name);
+        }
+
+        //Mutator
+        public void CaculateTax()
+        {
+            tax = salary * 0.16;
+        }
+    }
+}
---
+++ Accesors/Program.cs
@@ -0,0 +1,13 @@
+﻿using Accessor;
+
+Employee employee = new Employee();
+employee.Show();
+
+employee.SetSalary(40000);
+employee.SetName("Juan");
+
+employee.Show();
+
+employee.CaculateTax();
+
+employee.Show();
