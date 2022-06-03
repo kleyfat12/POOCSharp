9b036a5b49516d1e5a40befa68cd764536be20e4
Fernando Carrillo Lopez <kleyfat12@gmail.com>
2022-06-03 09:12:15 +0200

    Overloading

---
+++ Overloading/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Calculator
+    {
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public double Sum(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ Overloading/Program.cs
@@ -0,0 +1,5 @@
+﻿using Overloading;
+
+Calculator calculator = new Calculator();
+Console.WriteLine(calculator.Sum(12, 24));
+Console.WriteLine(calculator.Sum(12, 24, 36));
