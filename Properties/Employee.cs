5a6c430ca0e5b94f6099c9193dbe2330edd50ae0
Fernando Carrillo Lopez <kleyfat12@gmail.com>
2022-06-03 08:35:47 +0200

    Properties

---
+++ Properties/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Employee
+    {
+        private double salary;
+        private string name;
+
+        private string log;
+
+        public double Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (salary < 5000 || salary > 50000)
+                {
+                    LogError("SetSalary failure");
+                    return;
+                }
+                salary = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        private void LogError(string error)
+        {
+            log += error + "\n";
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Salary = {0}, Name = {1}", Salary, Name);
+        }
+        public void ShowLog()
+        {
+            Console.WriteLine(log);
+        }
+
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,8 @@
+﻿using Properties;
+
+Employee employee = new Employee();
+employee.Salary = 500000;
+employee.Name = "Fernando";
+
+employee.Show();
+employee.ShowLog();
