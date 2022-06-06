03b63751160eb3d753b00385949e541ece662c6c
Fernando Carrillo Lopez <kleyfat12@gmail.com>
2022-06-06 10:52:03 +0200

    Static Methods

---
+++ StaticMethods/Printer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethods
+{
+    class Printer
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        public void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
---
+++ StaticMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using StaticMethods;
+
+// SayHello is static so I can access through the class 
+Printer.SayHello();
+
+//I can not access to SayGoodbye through the class
+//Printer.SayGoodbye();
+
+// I need to create an object
+Printer printer = new Printer();
+printer.SayGoodbye();
+
+// I can not access to SayHello through an object
+// printer.SayHello();
