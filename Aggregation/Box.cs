02ecbe4e0e415401161b201fc28fd46e309efb62
Fernando Carrillo Lopez <kleyfat12@gmail.com>
2022-06-06 16:26:39 +0200

    Aggregation

---
+++ Aggregation/Box.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aggregation
+{
+    class Box
+    {
+        private int number;
+        private Ball? ball = null;
+
+        private static int counter;
+
+        public int Number { get { return number; } }
+        public Ball Ball
+        {
+            get { return ball; }
+            set
+            {
+                if (ball != null)
+                {
+                    return;
+                }
+                ball = value;
+            }
+        }
+
+        public Box()
+        {
+            number = ++counter;
+        }
+
+        public void Show()
+        {
+            if (ball != null)
+            {
+                Console.WriteLine($"A box with a {Ball.Color} ball");
+            }
+            else
+            {
+                Console.WriteLine("There is no ball in the box");
+            }
+        }
+    }
+}
---
+++ Aggregation/Program.cs
@@ -0,0 +1,15 @@
+﻿using Aggregation;
+
+Box box = new Box();
+Ball blueBall = new Ball("Blue");
+Ball redBall = new Ball("Red");
+
+box.Show();
+
+box.Ball = blueBall;
+
+box.Show();
+
+box.Ball = redBall;
+
+box.Show();
