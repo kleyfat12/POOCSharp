d7458f9b4834f30885824d13e7ed8e287751b988
Fernando Carrillo Lopez <kleyfat12@gmail.com>
2022-06-06 10:34:43 +0200

    Static Attributes

---
+++ StaticAttributes/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticAttributes
+{
+    class Employee
+    {
+        private int number;
+        /*
+         * The static attribute is shared by all objects. 
+         */
+        private static int counter;
+
+        public string Name { get; set; }
+        public int Number { get { return number; } }
+        public Employee(string name)
+        {
+            Name = name;
+            number = ++counter;
+        }
+    }
+}
---
+++ StaticAttributes/Program.cs
@@ -0,0 +1,13 @@
+﻿using StaticAttributes;
+
+Employee fer = new Employee("Fer");
+Employee cesar = new Employee("Cesar");
+Employee patrick = new Employee("Patrick");
+
+/*
+ * As the attribute is shared, counter is incremented 
+ * with each new instance and the value is assigned to Number
+ */
+Console.WriteLine(fer.Number);
+Console.WriteLine(cesar.Number);
+Console.WriteLine(patrick.Number);
