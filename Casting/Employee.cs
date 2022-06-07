2e172eca4db10d15b190cbd1ef2295b1871758ba
Fernando Carrillo Lopez <kleyfat12@gmail.com>
2022-06-07 16:16:51 +0200

    Casting

---
+++ Casting/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+    class Employee : Person
+    {
+        public override void SayHello()
+        {
+            Console.WriteLine("HELLOOOOO!");
+        }
+
+        public void EmployeeSayHello()
+        {
+            Console.WriteLine("Hello from employee");
+        }
+    }
+}
---
+++ Casting/Program.cs
@@ -0,0 +1,30 @@
+﻿using Casting;
+
+Person person = new Employee();
+
+/*
+ * Even though the variable is actually of the "Employee" type,
+ * I can not access to EmployeeSayHello method
+ */
+// person.EmployeeSayHello();
+
+/*
+ * We need to cast to Employee
+ */
+((Employee)person).EmployeeSayHello();
+
+/*
+ * object is the super class
+ */
+object person2 = new Employee();
+((Employee)person2).EmployeeSayHello();
+
+Console.WriteLine(person is object);
+Console.WriteLine(person is Employee);
+Console.WriteLine(person is Person);
+
+/*
+ * Notes:
+ * I only can cast from the more specific to the more generic
+ * Example: I can cast from Employee to Person
+ */
